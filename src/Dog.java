842d2c72be4edec2e09d6b9b6dd4e884053a57f2
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2025-10-02 14:51:39 -0700

    Added Dog.java and Edited SayApp.java

---
+++ src/Dog.java
@@ -0,0 +1,20 @@
+class Dog implements Animal {
+
+  @Override
+  public String getAnimalArt(){
+    return 
+By Maija Haavisto https://www.asciiart.eu/animals/dogs
+
+"        \\   .\n" +
+"         \\  ..^____/\n" +
+"            `-. ___ )\n" +
+"              ||   ||\n";
+  }
+
+
+@Override
+public String toString(){
+  return "s";
+}
+
+}
